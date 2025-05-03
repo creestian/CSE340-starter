fa01536fc974c39197ed29056957a33d290e9b0b
creestian <creestian@gmail.com>
2025-05-03 19:54:19 -0300

    W02 Prepare: EJS Templates Activity
    Overview
    You'll be working on using EJS templates to build a working server-side app in this activity.
    
    Instructions
    Complete the following steps:
    
    Download and Implement Server CodeLinks to an external site.
    Review the Starter FilesLinks to an external site.
    Build the applicationLinks to an external site.
    EJS View EngineLinks to an external site.
    Implement index route with Express and EJSLinks to an external site.
    Create a Render.com AccountLinks to an external site.
    Add a render.com application serviceLinks to an external site.
    DATABASE LOGIN INFO Download DATABASE LOGIN INFOfile. You will need this during this step.
    Deploy updates and test the applicationLinks to an external site.

--- server.js
+++ server.js
@@ -6,15 +6,23 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
  * Routes
+ * View Engine and Templates
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
-
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
